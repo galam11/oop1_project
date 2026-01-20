2ee9cc016d1dc5fe727c5f1d802db482e482f8fe
Gal Amar <gal143628@gmail.com>
2026-01-20 13:35:19 +0200

    feat: Implement font loading and retrieval in AssetsManager using 'Jersey10-Regular.ttf'.

--- include/AssetsManager.h
+++ include/AssetsManager.h
@@ -1,35 +1,33 @@
 #pragma once
-#include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
 
 #include <map>
 #include <string>
 
 #include "macros.h"
 
-
-class AssetsManager
-{
+class AssetsManager {
 public:
-    ~AssetsManager() = default;
+  ~AssetsManager() = default;
 
-    static AssetsManager& instance();
-    const sf::Texture& getTexture(const Types id) const;
-    const sf::SoundBuffer* getSoundBuffer(const char id) const;
+  static AssetsManager &instance();
+  const sf::Texture &getTexture(const Types id) const;
+  const sf::SoundBuffer *getSoundBuffer(const char id) const;
+  const sf::Font &getFont() const;
 
 private:
-    AssetsManager();
-    AssetsManager(const AssetsManager&) = default;
-    AssetsManager& operator=(const AssetsManager&) = default;
-    static AssetsManager m_instance;
+  AssetsManager();
+  AssetsManager(const AssetsManager &) = default;
+  AssetsManager &operator=(const AssetsManager &) = default;
+  static AssetsManager m_instance;
 
+  sf::Texture loadTexture(const std::string &filename);
+  sf::SoundBuffer loadSoundBuffer(const std::string &filename);
 
-    sf::Texture loadTexture(const std::string& filename);
-    sf::SoundBuffer loadSoundBuffer(const std::string& filename);
+  std::map<Types, sf::Texture> m_textures;
+  std::map<char, sf::SoundBuffer> m_soundBuffers;
 
-
-    std::map<Types, sf::Texture> m_textures;
-    std::map<char, sf::SoundBuffer> m_soundBuffers;
-
-	sf::Texture m_errTexture;
+  sf::Texture m_errTexture;
+  sf::Font m_font;
 };
--- src/AssetsManager.cpp
+++ src/AssetsManager.cpp
@@ -1,63 +1,59 @@
 #include "AssetsManager.h"
 #include "macros.h"
-#include <iostream>
 #include <SFML/Graphics.hpp>
+#include <iostream>
 #include <map>
 #include <string>
 
 AssetsManager AssetsManager::m_instance = AssetsManager();
 
-AssetsManager& AssetsManager::instance()
-{
-    return m_instance;
+AssetsManager &AssetsManager::instance() { return m_instance; }
+
+AssetsManager::AssetsManager() {
+  m_textures[PLAYER] = loadTexture("player.png");
+  m_textures[ENEMY] = loadTexture("enemy.png");
+  m_textures[COIN] = loadTexture("coin.png");
+  m_textures[FLOOR] = loadTexture("floor.png");
+  m_textures[BREAKABLE_FLOOR] = loadTexture("breakable_floor.png");
+  m_textures[LADDER] = loadTexture("ladder.png");
+  m_textures[RAIL] = loadTexture("rail.png");
+
+  m_errTexture = loadTexture("error.png");
+
+  if (!m_font.loadFromFile("jersey10-Regular.ttf")) {
+    std::cout << "Error loading font" << std::endl;
+  }
 }
 
-AssetsManager::AssetsManager()
-{
-    m_textures[PLAYER] = loadTexture("player.png");
-    m_textures[ENEMY] = loadTexture("enemy.png");
-    m_textures[COIN] = loadTexture("coin.png");
-    m_textures[FLOOR] = loadTexture("floor.png");
-    m_textures[BREAKABLE_FLOOR] = loadTexture("breakable_floor.png");
-    m_textures[LADDER] = loadTexture("ladder.png");
-    m_textures[RAIL] = loadTexture("rail.png");
-
-	m_errTexture = loadTexture("error.png");
+const sf::Texture &AssetsManager::getTexture(const Types id) const {
+  auto it = m_textures.find(id);
+  if (it != m_textures.end())
+    return (it->second);
+  return m_errTexture;
 }
 
-const sf::Texture& AssetsManager::getTexture(const Types id) const
-{
-    auto it = m_textures.find(id);
-    if (it != m_textures.end())
-        return (it->second);
-    return m_errTexture;
-}
+const sf::Font &AssetsManager::getFont() const { return m_font; }
 
 // temp
-const sf::SoundBuffer* AssetsManager::getSoundBuffer(const char id) const
-{
-    auto it = m_soundBuffers.find(id);
-    if (it != m_soundBuffers.end())
-        return &(it->second);
-    return nullptr;
+const sf::SoundBuffer *AssetsManager::getSoundBuffer(const char id) const {
+  auto it = m_soundBuffers.find(id);
+  if (it != m_soundBuffers.end())
+    return &(it->second);
+  return nullptr;
 }
 
-sf::Texture AssetsManager::loadTexture(const std::string& filename)
-{
-    sf::Texture temp;
-    if (!temp.loadFromFile(filename)) 
-    {
-        std::cerr << "Error loading :" << filename << std::endl;
-    }
-    return temp;
+sf::Texture AssetsManager::loadTexture(const std::string &filename) {
+  sf::Texture temp;
+  if (!temp.loadFromFile(filename)) {
+    std::cerr << "Error loading :" << filename << std::endl;
+  }
+  return temp;
 }
 
-sf::SoundBuffer AssetsManager::loadSoundBuffer(const std::string& filename)
-{
-    sf::SoundBuffer temp;
-    if (!temp.loadFromFile(filename))
-    {
-        std::cerr << "Error loading :" << filename << std::endl;
-    }
-    return temp;
+sf::SoundBuffer AssetsManager::loadSoundBuffer(const std::string &filename) {
+  sf::SoundBuffer temp;
+  if (!temp.loadFromFile(filename)) {
+    std::cerr << "Error loading :" << filename << std::endl;
+  }
+  return temp;
 }
